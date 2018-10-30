54f7292c95e0c6715134c02104768a129d565192
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-30 08:17:38 -0400

    added a json file, pulled from the controller

--- AngularJS/myCtrl.js
+++ AngularJS/myCtrl.js
@@ -1,4 +1,9 @@
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function($scope, $http) {
   $scope.firstName = "Aaron";
   $scope.lastName = "Ireland";
+
+  $http.get("json_sample.html").then(function (response) {
+
+    $scope.myData = response.data;
+  });
 });
