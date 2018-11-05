50c2b7fc5b8441759276fabf9ab4cd54bda3e199
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-05 11:13:44 -0500

    test

--- AngularJS/table.php
+++ AngularJS/table.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+
 $conn = new mysqli("localhost", "aaron", "southhills#", "aaron");
 
 $result = $conn->query("SELECT first_name, last_name, city FROM angular_people");
