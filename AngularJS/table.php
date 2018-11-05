0caab916947261f3c7e839eee640f86f816175df
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-05 09:07:16 -0500

    test

--- AngularJS/table.php
+++ AngularJS/table.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 $conn = new mysqli("localhost", "aaron", "southhills#", "aaron");
-$result = $conn->query("SELECT userid, first_name, last_name, city FROM angular_people");
+$result = $conn->query("SELECT first_name, last_name, city FROM angular_people");
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
