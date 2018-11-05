29f176eadd45ed28ba770700ff1d0c3e59db89fe
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-05 09:01:21 -0500

    test

--- AngularJS/myCtrl.js
+++ AngularJS/myCtrl.js
@@ -1,10 +1,4 @@
 app.controller("myCtrl", function($scope, $http) {
-    $scope.firstName = "Matthew";
-    $scope.lastName= "Penning";
-
-    $http.get("json_sample.html").then(function (response) {
-      $scope.myData = response.data.records;
-    });
 
     $http.get("json_info.php").then(function (response) {
       $scope.myPHPData = response.data.records;
