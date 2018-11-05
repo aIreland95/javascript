7b80b36814cef756b252aa47af11974269d4761d
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-05 09:03:38 -0500

    test

--- AngularJS/myCtrl.js
+++ AngularJS/myCtrl.js
@@ -1,6 +1,6 @@
 app.controller("myCtrl", function($scope, $http) {
 
-    $http.get("json_info.php").then(function (response) {
+    $http.get("table.php").then(function (response) {
       $scope.myPHPData = response.data.records;
     })
 });
