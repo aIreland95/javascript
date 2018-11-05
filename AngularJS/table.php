c87140e681f9eb0e752175da9581529cd169215c
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-05 08:14:10 -0500

    test

--- AngularJS/table.php
+++ AngularJS/table.php
@@ -2,21 +2,19 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new msqli("localhost", "aaron", "southhills#", "aaron");
+$conn = new mysqli("localhost", "aaron", "southhills#", "aaron");
 
-$result = $conn->query("SELECT * FROM aaron.angular_people");
+$result = $conn->query("SELECT first_name, last_name, city FROM angular_people");
 
-$output = "";
-
-while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-  if ($output != "") {$output .= ",";}
-  $output .= '{"First Name":"'  . $row["first_name"] . '",';
-  $output .= '"Last Name":"'   . $row["last_name"]        . '",';
-  $output .= '"City":"'. $row["city"]     . '"}';
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"First Name":"'  . $rs["first_name"] . '",';
+    $outp .= '"Last Name":"'   . $rs["last_name"]        . '",';
+    $outp .= '"City":"'. $rs["city"]     . '"}';
 }
-$output = '{"Angular People":[' .$output.']}';
+$outp ='{"Angular People":['.$outp.']}';
 $conn->close();
 
-echo($output);
-
+echo($outp);
 ?>
